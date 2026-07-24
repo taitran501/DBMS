9df2088c01e1dd627f4f2f4fae2133129abc5045
taitran501 <trantantai7766@gmail.com>
2026-07-24 11:07:48 +0700

    feat(database-object): implement ViewBuilder class

--- src/dbms/database_object/__init__.py
+++ src/dbms/database_object/__init__.py
@@ -14,6 +14,7 @@ from dbms.database_object.index_factory import (
 )
 from dbms.database_object.table_builder import TableBuilder
 from dbms.database_object.trigger import Trigger
+from dbms.database_object.view_builder import ViewBuilder
 
 __all__ = [
     "BTreeIndexFactory",
@@ -26,5 +27,6 @@ __all__ = [
     "TableBuilder",
     "TextDataTypeFactory",
     "Trigger",
+    "ViewBuilder",
 ]
 
